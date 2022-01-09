37b1e9bf8e9f92c7b1ed45065905fd828813625a
renasami <drrr0502@gmail.com>
2022-01-09 18:55:05 +0900

    store 途中

---
+++ front/src/UploadImg.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let file
+    let src
+	const reader = new FileReader();
+	reader.onload = () => {
+        src = reader.result;
+	};
+	reader.onerror = (error) => {
+	  console.log('Error: ', error);
+	};
+	const convertToBase64 = () => {
+		reader.readAsDataURL(file[0]);
+	}
+
+</script>
+<div>
+	<input id="1" bind:files={file} type="file" on:change={convertToBase64}/>
+</div>
+
+<style>
+	div {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+
+</style>
