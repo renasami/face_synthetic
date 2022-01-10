f89e5eb240b9472e666b3e08dea86c5c1bf1c747
renasami <drrr0502@gmail.com>
2022-01-11 02:16:21 +0900

    あとは連携すれば完成

--- api/main.py
+++ api/main.py
@@ -3,6 +3,11 @@ import base64
 import cv2
 import numpy as np
 import random
+import datetime
+import os
+import facemorpher
+import glob
+import binascii
 
 app = FastAPI()
 
@@ -10,14 +15,36 @@ app = FastAPI()
 def root():
     return {"message":"world"}
 
-print(cv2.__version__)
+
+def generate_syntheticed_faces(files:list,folder) -> base64:
+    output = "{}/result.png".format(folder)
+    facemorpher.averager(files,background='transparent',out_filename=output,plot=False)
+    img = open(output, "rb")
+    return base64.b64encode(img.read())
+
+
 
 @app.post("/img_save")
 def save_img(imgs):
-
+    print(type(imgs))
+    imgs = eval(imgs)
+    if len(imgs) == 0:
+        return False
+    date = datetime.datetime.now().strftime("%Y%m%d%H%M")
     rand = random.random()
-    image_file='{}decode.png'.format(rand)
-    binary = base64.b64decode(imgs)
-    with open(image_file, 'bw') as f4:
-        f4.write(binary)
-    # return binary
+    img_folder = "tmp/{}{}".format(date,rand)
+    os.mkdir(img_folder)
+    for i,img in enumerate(imgs):
+        image_file="./{}/{}.png".format(img_folder,i)
+        binary = base64.b64decode(img)
+        # f = open(image_file,"bw")
+        # f.write(binary)
+        # f.close()
+        
+        with open(image_file, 'bw') as f4:
+            f4.write(binary)        
+        files = ["{}/{}.png".format(img_folder,n) for n in range(2)]
+    result = generate_syntheticed_faces(files,img_folder)
+
+    return result.decode("utf-8")
+
